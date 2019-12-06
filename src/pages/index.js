a6b72746d83010a3237fd729e90fbe1ac06d9b85
Ahmed <ahmedfaaid.coding@gmail.com>
2019-12-05 21:45:41 -0500

    Remove static query and added graphql aliases

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,36 @@
 import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import { graphql, StaticQuery } from 'gatsby'
+import { graphql } from 'gatsby'
 
 import Review from '../components/Reviews/Review'
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
     <Layout>
         <SEO title='Home' />
-        <StaticQuery
-            query={indexReviewsQuery}
-            render={data => {
-                return (
-                    <div className='my-3'>
-                        <h2 className='font-heading text-xl text-center md:text-left subpixel-antialiased font-bold tracking-wide mb-3 p-3'>
-                            Latest Reviews
-                        </h2>
-                        <div className='block md:flex mb-4 justify-between flex-wrap'>
-                            {data.allMarkdownRemark.edges.map(({ node }) => (
-                                <Review
-                                    key={node.id}
-                                    title={node.frontmatter.title}
-                                    path={node.frontmatter.path}
-                                    fluid={
-                                        node.frontmatter.featuredImage
-                                            .childImageSharp.fluid
-                                    }
-                                />
-                            ))}
-                        </div>
-                    </div>
-                )
-            }}
-        />
+        <div className='my-3'>
+            <h2 className='font-heading text-xl text-center md:text-left subpixel-antialiased font-bold tracking-wide mb-3 p-3'>
+                Latest Reviews
+            </h2>
+            <div className='block md:flex mb-4 justify-between flex-wrap'>
+                {data.reviews.edges.map(({ node }) => (
+                    <Review
+                        key={node.id}
+                        title={node.frontmatter.title}
+                        path={node.frontmatter.path}
+                        fluid={
+                            node.frontmatter.featuredImage.childImageSharp.fluid
+                        }
+                    />
+                ))}
+            </div>
+        </div>
     </Layout>
 )
 
-const indexReviewsQuery = graphql`
-    query indexReviewsQuery {
-        allMarkdownRemark(
+export const indexReviewsQuery = graphql`
+    query {
+        reviews: allMarkdownRemark(
             filter: { fileAbsolutePath: { regex: "/(reviews)/.*\\\\.md$/" } }
             sort: { fields: [frontmatter___date], order: DESC }
             limit: 3
