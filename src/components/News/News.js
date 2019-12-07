1350ddf6dbe1585e9c8d31cd360f079d62d2193f
Ahmed <ahmedfaaid.coding@gmail.com>
2019-12-07 12:16:08 -0500

    Add hyperlink to card images

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -5,7 +5,9 @@ import Img from 'gatsby-image'
 const News = ({ title, path, fluid }) => {
     return (
         <div className='lg:max-w-xs overflow-hidden md:w-2/5 lg:w-1/3 mx-3 mb-3 shadow-md md:shadow-none'>
-            <Img className='w-full md:w-10/12 mx-auto' fluid={fluid} />
+            <Link to={path}>
+                <Img className='w-full md:w-10/12 mx-auto' fluid={fluid} />
+            </Link>
             <div className='md:px-6 py-4'>
                 <h3 className='font-heading text-lg subpixel-antialiased mb-2'>
                     {title}
--- src/components/Reviews/Review.js
+++ src/components/Reviews/Review.js
@@ -4,15 +4,17 @@ import Img from 'gatsby-image'
 
 const Review = ({ title, path, fluid }) => {
     return (
-        <div className="lg:max-w-xs overflow-hidden md:w-2/5 lg:w-1/3 mx-3 mb-3 shadow-md md:shadow-none">
-            <Img className="w-full md:w-10/12 mx-auto" fluid={fluid} />
-            <div className="md:px-6 py-4">
-                <h3 className="font-heading text-lg subpixel-antialiased mb-2">
+        <div className='lg:max-w-xs overflow-hidden md:w-2/5 lg:w-1/3 mx-3 mb-3 shadow-md md:shadow-none'>
+            <Link to={path}>
+                <Img className='w-full md:w-10/12 mx-auto' fluid={fluid} />
+            </Link>
+            <div className='md:px-6 py-4'>
+                <h3 className='font-heading text-lg subpixel-antialiased mb-2'>
                     {title}
                 </h3>
             </div>
             <Link
-                className="text-primary hover:text-secondary hover:underline md:ml-6"
+                className='text-primary hover:text-secondary hover:underline md:ml-6'
                 to={path}
             >
                 Read more <span>&rarr;</span>
