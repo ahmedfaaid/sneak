8b6780ea73a3909bff70baf30794aad1dfa65beb
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2019-12-23 22:00:35 -0500

    Edit 404 error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,18 @@
-import React from "react"
+import React from 'react'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <Layout>
+        <SEO title='404: Not found' />
+        <h1 className='font-heading text-xl text-center md:text-left subpixel-antialiased font-bold tracking-wide mb-3 p-3'>
+            NOT FOUND
+        </h1>
+        <p className='font-body text-lg text-secondary'>
+            Oops this page does not exist
+        </p>
+    </Layout>
 )
 
 export default NotFoundPage
