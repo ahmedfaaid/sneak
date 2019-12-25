6c2a308a2534a4006e69988eed5200a8cd93cb70
Ahmed <ahmedfaaid.coding@gmail.com>
2019-12-25 16:31:14 -0500

    Create AdComponent as an ad placeholder

---
+++ src/components/AdComponent/AdComponent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const AdComponent = () => {
+    const styles = {
+        width: '300px',
+        height: '250px',
+        background: '#91e5f6',
+        color: '#fff',
+        lineHeight: '250px',
+        textAlign: 'center' 
+    }
+
+    return (
+        <div style={styles}>
+            AD SPACE
+        </div>
+    )
+};
+
+export default AdComponent;
