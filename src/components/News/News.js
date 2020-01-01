866ef7f1d40bbfa1cccfb18460922a75eb975952
Ahmed <ahmedfaaid.coding@gmail.com>
2019-12-31 21:08:30 -0500

    Add card image heights

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Img from 'gatsby-image'
 import PropTypes from 'prop-types'
+import './News.css'
 
 const News = ({ title, slug, fluid }) => {
     return (
         <div className='lg:max-w-xs xl:max-w-sm overflow-hidden w-full md:w-2/5 lg:w-64 xl:w-2/4 md:mx-3 mb-3 shadow-md md:shadow-none'>
             <Link to={slug}>
-                <Img className='w-full md:w-10/12 mx-auto' fluid={fluid} />
+                <Img
+                    className='w-full md:w-10/12 mx-auto image-height'
+                    fluid={fluid}
+                />
             </Link>
             <div className='md:px-6 py-4'>
                 <Link to={slug}>
