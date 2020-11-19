ffb2df0b37d440ba743cb67ed3cd42b64a92995b
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2020-11-19 13:45:53 -0500

    remove gatsby-source-tsdb plugin

--- src/pages/releases.js
+++ src/pages/releases.js
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const Releases = () => (
-    <Layout>
-        <SEO title='Releases' description='Find out about upcoming and past sneaker releases. We bring you information about Jordans, Lebrons, Yeezys etc.' />
-        <div className='my-3'>
-            <h1 className='font-heading text-xl text-center md:text-left subpixel-antialiased font-bold tracking-wide mb-3 p-3'>
-                Release Dates
-            </h1>
-            <p className='font-body text-xl text-gray-700 text-center mb-10'>
-                Upcoming sneaker releases
-            </p>
-            <hr />
-            <p className='font-body font-bold text-xl mt-6 p-3'>Coming soon</p>
-        </div>
-    </Layout>
-)
+const Releases = () => {
+    return (
+        <Layout>
+            <SEO
+                title='Releases'
+                description='Find out about upcoming and past sneaker releases. We bring you information about Jordans, Lebrons, Yeezys etc.'
+            />
+            <div className='my-3'>
+                <h1 className='font-heading text-xl text-center md:text-left subpixel-antialiased font-bold tracking-wide mb-3 p-3'>
+                    Release Dates
+                </h1>
+                <p className='font-body text-xl text-gray-700 text-center mb-10'>
+                    Upcoming sneaker releases
+                </p>
+                <hr />
+                <p className='font-body font-bold text-xl mt-6 p-3'>
+                    Coming soon
+                </p>
+            </div>
+        </Layout>
+    )
+}
 
 export default Releases
