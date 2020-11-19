78c2134e558c03bd69fa493aa4a16a2eaf9500ee
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2020-11-18 21:20:52 -0500

    add tsdb plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,6 +67,7 @@ module.exports = {
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
         // `gatsby-plugin-offline`,
-        `gatsby-plugin-postcss`
+        `gatsby-plugin-postcss`,
+        `gatsby-source-tsdb`
     ]
 }
