01e44d91ed3db14c2a3e62b13b15cfd8461f321c
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2020-11-19 21:44:04 -0500

    fix slug routing

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -5,35 +5,32 @@ import PropTypes from 'prop-types'
 import './Post.css'
 
 const Post = ({ title, slug, fluid }) => {
-    return (
-        <div className='lg:max-w-xs xl:max-w-sm overflow-hidden w-full md:w-2/5 lg:w-64 xl:w-2/4 md:mx-3 mb-3 shadow-md md:shadow-none'>
-            <Link to={slug}>
-                <Img
-                    className='w-full md:w-10/12 mx-auto image-height'
-                    fluid={fluid}
-                />
-            </Link>
-            <div className='md:px-6 py-4'>
-                <Link to={slug}>
-                    <h1 className='font-heading text-lg subpixel-antialiased mb-2'>
-                        {title}
-                    </h1>
-                </Link>
-            </div>
-            <Link
-                className='text-primary hover:text-secondary hover:underline md:ml-6'
-                to={slug}
-            >
-                Read more <span>&rarr;</span>
-            </Link>
-        </div>
-    )
+  return (
+    <div className='lg:max-w-xs xl:max-w-sm overflow-hidden w-full md:w-2/5 lg:w-64 xl:w-2/4 md:mx-3 mb-3 shadow-md md:shadow-none'>
+      <Link to={`/${slug}`}>
+        <Img className='w-full md:w-10/12 mx-auto image-height' fluid={fluid} />
+      </Link>
+      <div className='md:px-6 py-4'>
+        <Link to={`/${slug}`}>
+          <h1 className='font-heading text-lg subpixel-antialiased mb-2'>
+            {title}
+          </h1>
+        </Link>
+      </div>
+      <Link
+        className='text-primary hover:text-secondary hover:underline md:ml-6'
+        to={`/${slug}`}
+      >
+        Read more <span>&rarr;</span>
+      </Link>
+    </div>
+  )
 }
 
 Post.propTypes = {
-    title: PropTypes.string.isRequired,
-    slug: PropTypes.string.isRequired,
-    fluid: PropTypes.object.isRequired
+  title: PropTypes.string.isRequired,
+  slug: PropTypes.string.isRequired,
+  fluid: PropTypes.object.isRequired
 }
 
 export default Post
