bc935a4f8f1bcac4d25cbeaddde105876d249c3b
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2020-11-22 12:54:34 -0500

    remove instagram and sidebar temporarily

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,18 +1,16 @@
 import React from 'react'
-import Instagram from '../Instagram/Instagram'
 // import SubscribeForm from '../SubscribeForm/SubscribeForm'
 import AdComponent from '../AdComponent/AdComponent'
 
 const Sidebar = () => (
-    <aside className='sm:max-w-md md:min-h-screen md:top-0 my-6 bg-gray-200'>
-        <div className='p-2'>
-            <AdComponent />
-            {/* add subscribe form when ready for newsletter subscriptions */}
-            {/* <SubscribeForm /> */}
-            <hr />
-            <Instagram />
-        </div>
-    </aside>
+  <aside className='sm:max-w-md md:min-h-screen md:top-0 my-6 bg-gray-200'>
+    <div className='p-2'>
+      <AdComponent />
+      {/* add subscribe form when ready for newsletter subscriptions */}
+      {/* <SubscribeForm /> */}
+      <hr />
+    </div>
+  </aside>
 )
 
 export default Sidebar
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,54 +1,47 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from 'react'
 import PropTypes from 'prop-types'
 import { useStaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
 import Footer from './Footer/Footer'
-import Sidebar from './Sidebar/Sidebar'
+// import Sidebar from './Sidebar/Sidebar'
 import './layout.css'
 
 import favicon from '../images/favicon.ico'
 import Helmet from 'react-helmet'
 
 const Layout = ({ children }) => {
-    const data = useStaticQuery(graphql`
-        query SiteTitleQuery {
-            site {
-                siteMetadata {
-                    title
-                }
-            }
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
         }
-    `)
+      }
+    }
+  `)
 
-    return (
-        <>
-            <Helmet>
-                <link rel='icon' href={favicon} />
-                <script
-                    src='https://kit.fontawesome.com/163b6190b4.js'
-                    crossorigin='anonymous'
-                ></script>
-            </Helmet>
-            <Header siteTitle={data.site.siteMetadata.title} />
-            <div className='container md:flex push-footer'>
-                <main className='w-full md:w-4/5 mx-auto mr-4'>{children}</main>
-                <Sidebar />
-            </div>
-            <Footer />
-        </>
-    )
+  return (
+    <>
+      <Helmet>
+        <link rel='icon' href={favicon} />
+        <script
+          src='https://kit.fontawesome.com/163b6190b4.js'
+          crossorigin='anonymous'
+        ></script>
+      </Helmet>
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <div className='container md:flex push-footer'>
+        <main className='w-full md:w-4/5 mx-auto'>{children}</main>
+        {/* <Sidebar /> */}
+      </div>
+      <Footer />
+    </>
+  )
 }
 
 Layout.propTypes = {
-    children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired
 }
 
 export default Layout
