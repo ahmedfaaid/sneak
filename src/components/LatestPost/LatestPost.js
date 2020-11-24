339a75a64a3f8bd57322c7455f4f67c362259216
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2020-11-24 14:16:46 -0500

    fix image widths, heights and quality

--- src/components/LatestPost/LatestPost.js
+++ src/components/LatestPost/LatestPost.js
@@ -4,68 +4,65 @@ import { graphql, StaticQuery } from 'gatsby'
 import Post from '../Post/Post'
 
 const LatestPost = () => (
-    <StaticQuery
-        query={latestPostQuery}
-        render={data => {
-            return (
-                <div className='my-3'>
-                    <h2 className='font-heading text-xl text-center md:text-left subpixel-antialiased font-bold tracking-wide mb-3 p-3'>
-                        Latest Posts
-                    </h2>
-                    <div className='block md:flex mb-4 justify-between flex-wrap'>
-                        {data.allMarkdownRemark.edges.map(({ node }) => (
-                            <>
-                                {node.frontmatter.publish ? (
-                                    <Post
-                                        key={node.id}
-                                        title={node.frontmatter.title}
-                                        slug={node.fields.slug}
-                                        fluid={
-                                            node.frontmatter.featuredImage
-                                                .childImageSharp.fluid
-                                        }
-                                    />
-                                ) : null}
-                            </>
-                        ))}
-                    </div>
-                </div>
-            )
-        }}
-    />
+  <StaticQuery
+    query={latestPostQuery}
+    render={data => {
+      return (
+        <div className='my-3'>
+          <h2 className='font-heading text-xl text-center md:text-left subpixel-antialiased font-bold tracking-wide mb-3 p-3'>
+            Latest Posts
+          </h2>
+          <div className='block md:flex mb-4 justify-between flex-wrap'>
+            {data.allMarkdownRemark.edges.map(({ node }) => (
+              <>
+                {node.frontmatter.publish ? (
+                  <Post
+                    key={node.id}
+                    title={node.frontmatter.title}
+                    slug={node.fields.slug}
+                    fluid={node.frontmatter.featuredImage.childImageSharp.fluid}
+                  />
+                ) : null}
+              </>
+            ))}
+          </div>
+        </div>
+      )
+    }}
+  />
 )
 
 export const latestPostQuery = graphql`
-    query {
-        allMarkdownRemark(
-            filter: {
-                fileAbsolutePath: { regex: "/(news|reviews)/.*\\\\.md$/" }
-                frontmatter: { publish: { eq: true } }
-            }
-            sort: { fields: [frontmatter___date], order: DESC }
-            limit: 4
-        ) {
-            edges {
-                node {
-                    id
-                    frontmatter {
-                        title
-                        featuredImage {
-                            childImageSharp {
-                                fluid(maxWidth: 800) {
-                                    ...GatsbyImageSharpFluid
-                                }
-                            }
-                        }
-                        publish
-                    }
-                    fields {
-                        slug
-                    }
+  query {
+    allMarkdownRemark(
+      filter: {
+        fileAbsolutePath: { regex: "/(news|reviews)/.*\\\\.md$/" }
+        frontmatter: { publish: { eq: true } }
+      }
+      sort: { fields: [frontmatter___date], order: DESC }
+      limit: 6
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            featuredImage {
+              childImageSharp {
+                fluid(maxWidth: 600) {
+                  ...GatsbyImageSharpFluid
                 }
+              }
             }
+            publish
+          }
+          fields {
+            slug
+          }
         }
+      }
     }
+  }
 `
 
 export default LatestPost
--- src/templates/singlePost.js
+++ src/templates/singlePost.js
@@ -7,72 +7,73 @@ import { slugify } from '../util/utilityFunctions'
 import './singlePost.css'
 
 const singlePost = ({ data }) => {
-    const post = data.markdownRemark.frontmatter
-    const image = post.featuredImage.childImageSharp.resize.src
-    return (
-        <Layout>
-            <SEO title={post.title} image={image} />
-            <h1 className='font-heading text-3xl text-center subpixel-antialiased font-bold tracking-wide my-3 p-3'>
-                {post.title}
-            </h1>
-            <hr />
-            <div className='w-full md:w-3/4 mx-auto mt-10 px-0 md:px-8'>
-                <Img fluid={post.featuredImage.childImageSharp.fluid} />
-                <div className='my-10 smMax:text-center'>
-                    <p className='mb-2 font-body text-base text-gray-500'>
-                        {post.date}
-                    </p>
-                    <h3 className='mb-3 font-body text-base text-white bg-primary inline-block p-1 rounded'>
-                        {post.author}
-                    </h3>
-                    <hr />
-                    <div
-                        className='news-link mt-10 mb-6 font-body leading-loose text-left'
-                        dangerouslySetInnerHTML={{
-                            __html: data.markdownRemark.html
-                        }}
-                    />
-                    <div className='px-6 py-4 text-left'>
-                        <span className='mr-3'>
-                            <i className='fas fa-tags mr-1'></i>Tags:
-                        </span>
-                        {post.tags.map(tag => (
-                            <Link to={`/tag/${slugify(tag)}`} key={tag}>
-                                <span className='inline-block bg-primary rounded-full px-1 sm:px-3 py-1 text-xs sm:text-sm font-semibold text-white mr-2'>
-                                    {tag}
-                                </span>
-                            </Link>
-                        ))}
-                    </div>
-                </div>
-            </div>
-        </Layout>
-    )
+  const post = data.markdownRemark.frontmatter
+  const image = post.featuredImage.childImageSharp.resize.src
+  return (
+    <Layout>
+      <SEO title={post.title} image={image} />
+      <h1 className='font-heading text-3xl text-center subpixel-antialiased font-bold tracking-wide my-3 p-3'>
+        {post.title}
+      </h1>
+      <hr />
+      <div className='w-full md:w-3/4 mx-auto mt-10 px-0 md:px-8'>
+        <Img
+          fluid={post.featuredImage.childImageSharp.fluid}
+          className='object-contain'
+        />
+        <div className='my-10 smMax:text-center'>
+          <p className='mb-2 font-body text-base text-gray-500'>{post.date}</p>
+          <h3 className='mb-3 font-body text-base text-white bg-primary inline-block p-1 rounded'>
+            {post.author}
+          </h3>
+          <hr />
+          <div
+            className='news-link mt-10 mb-6 font-body leading-loose text-left'
+            dangerouslySetInnerHTML={{
+              __html: data.markdownRemark.html
+            }}
+          />
+          <div className='px-6 py-4 text-left'>
+            <span className='mr-3'>
+              <i className='fas fa-tags mr-1'></i>Tags:
+            </span>
+            {post.tags.map(tag => (
+              <Link to={`/tag/${slugify(tag)}`} key={tag}>
+                <span className='inline-block bg-primary rounded-full px-1 sm:px-3 py-1 text-xs sm:text-sm font-semibold text-white mr-2'>
+                  {tag}
+                </span>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 export const postQuery = graphql`
-    query postBySlug($slug: String!) {
-        markdownRemark(fields: { slug: { eq: $slug } }) {
-            id
-            html
-            frontmatter {
-                title
-                author
-                date(formatString: "MMM Do YYYY")
-                tags
-                featuredImage {
-                    childImageSharp {
-                        fluid(maxWidth: 800, maxHeight: 600) {
-                            ...GatsbyImageSharpFluid
-                        }
-                        resize(width: 800) {
-                            src
-                        }
-                    }
-                }
+  query postBySlug($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      html
+      frontmatter {
+        title
+        author
+        date(formatString: "MMM Do YYYY")
+        tags
+        featuredImage {
+          childImageSharp {
+            fluid(maxWidth: 800, maxHeight: 500, quality: 80) {
+              ...GatsbyImageSharpFluid
             }
+            resize(width: 800) {
+              src
+            }
+          }
         }
+      }
     }
+  }
 `
 
 export default singlePost
